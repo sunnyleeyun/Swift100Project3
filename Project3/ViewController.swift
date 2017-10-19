cd9893d32cfe858f7d6cd3d8714db0fc10024697
Sunny <sunnylee945@hotmail.com>
2017-10-19 21:07:44 +0800

    done for all the project

--- Project3/ViewController.swift
+++ Project3/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+  var songPlayer = AVAudioPlayer()
+  var hasBeenPaused = false
+  func prepareSongAndSession(){
+    do{
+      // Insert the song from our Bundle to our AVAudioPlayer\
+      songPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "music1", ofType: "mp3")!))
+      // Prepare the song to be played
+      songPlayer.prepareToPlay()
+      // allow to change the rate of song
+      songPlayer.enableRate = true
+      // Create audio session
+      let audioSession = AVAudioSession.sharedInstance()
+      do {
+        try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+      } catch let sessionError {
+        print(sessionError)
+      }
+      
+    } catch let songPlayerError{
+      print(songPlayerError)
+    }
+  }
+  
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var speed_label: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    prepareSongAndSession()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func play_Button_Tapped(_ sender: Any) {
+    songPlayer.play()
+  }
+  
+  @IBAction func pause_Button_Tapped(_ sender: Any) {
+    if songPlayer.isPlaying{
+      songPlayer.pause()
+      hasBeenPaused = true
+    }else{
+      hasBeenPaused = false
+    }
+  }
+  
+  @IBAction func restart_Button_Tapped(_ sender: Any) {
+    if songPlayer.isPlaying || hasBeenPaused {
+      songPlayer.stop()
+      songPlayer.currentTime = 0
+      
+      songPlayer.play()
+    }else{
+      songPlayer.play()
+    }
+  }
+  
+  
+  @IBAction func slider_moved(_ sender: Any) {
+    speed_label.text = String(format: "%.2f", slider.value)
+    songPlayer.rate = slider.value
+  }
+  
 }
 
